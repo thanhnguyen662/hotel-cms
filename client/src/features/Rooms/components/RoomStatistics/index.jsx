3c7a3d0a1212bfa0c75bef5ef8fb0bb3366e3b26
thongdxgcd18684@fpt.edu.vn <SexyMeoww@DESKTOP-K1JGHBT>
2021-12-17 23:08:41 +0700

    fix-aglin-statitics-component

--- client/src/features/Rooms/components/RoomStatistics/index.jsx
+++ client/src/features/Rooms/components/RoomStatistics/index.jsx
@@ -1,4 +1,4 @@
-import { Box, HStack, StackDivider } from '@chakra-ui/react';
+import { Box, HStack, StackDivider, Flex } from '@chakra-ui/react';
 import React from 'react';
 // import PropTypes from 'prop-types';
 
@@ -7,32 +7,26 @@ RoomStatistics.propTypes = {};
 function RoomStatistics(props) {
    return (
       <>
-         <Box height='full' align={'center'} mx='6' my='10'>
-            <HStack
-               spacing={2}
-               align='center'
-               divider={<StackDivider borderColor='gray.200' />}
-            >
-               <Box flex='1' align='center'>
-                  <Box fontSize='0.8em'>SINGLE</Box>
-                  <Box fontSize='1.5em' fontWeight='bold'>
-                     5/17
-                  </Box>
+         <Flex justify='center' align='center' height='full' mx='10px'>
+            <Box flex='1' align='center'>
+               <Box fontSize='0.8em'>SINGLE</Box>
+               <Box fontSize='1.5em' fontWeight='bold'>
+                  5/17
                </Box>
-               <Box flex='1' align='center'>
-                  <Box fontSize='0.8em'>DOUBLE</Box>
-                  <Box fontSize='1.5em' fontWeight='bold'>
-                     5/17
-                  </Box>
+            </Box>
+            <Box flex='1' align='center'>
+               <Box fontSize='0.8em'>DOUBLE</Box>
+               <Box fontSize='1.5em' fontWeight='bold'>
+                  5/17
                </Box>
-               <Box flex='1' align='center'>
-                  <Box fontSize='0.8em'>VIP</Box>
-                  <Box fontSize='1.5em' fontWeight='bold'>
-                     5/17
-                  </Box>
+            </Box>
+            <Box flex='1' align='center'>
+               <Box fontSize='0.8em'>VIP</Box>
+               <Box fontSize='1.5em' fontWeight='bold'>
+                  5/17
                </Box>
-            </HStack>
-         </Box>
+            </Box>
+         </Flex>
       </>
    );
 }
