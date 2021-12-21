7c162d4a6ddfa97e0d858bf51ede56daa8169d67
thongdxgcd18684@fpt.edu.vn <SexyMeoww@DESKTOP-K1JGHBT>
2021-12-21 10:49:53 +0700

    Add event

---
+++ client/src/features/Event/components/AddEvent/index.jsx
@@ -0,0 +1,174 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import * as Yup from 'yup';
+import { Formik } from 'formik';
+import 'flatpickr/dist/themes/material_green.css';
+import Flatpickr from 'react-flatpickr';
+
+import {
+   Box,
+   Button,
+   Center,
+   FormControl,
+   FormErrorMessage,
+   FormLabel,
+   HStack,
+   Input,
+   NumberInput,
+   NumberInputField,
+   Select,
+   Stack,
+   useColorModeValue,
+   Textarea,
+} from '@chakra-ui/react';
+
+AddEvent.propTypes = {
+   onClose: PropTypes.func,
+};
+
+function AddEvent(props) {
+   //STATE
+   const [endLimit, setEndLimit] = useState(new Date());
+   //PROPS
+   const { onClose } = props;
+
+   //YUP
+   const validationSchema = Yup.object().shape({
+      name: Yup.string().required('Event name is required!'),
+      detail: Yup.string().required('Event detail is required!'),
+      start: Yup.date()
+         .min(new Date(), 'End date invalid')
+         .required('Start date invalid'),
+      end: Yup.date()
+         .min(endLimit, 'End date invalid')
+         .required('End date invalid'),
+   });
+
+   return (
+      <Stack
+         spacing={5}
+         w={'full'}
+         maxW={'md'}
+         bg={useColorModeValue('white', 'gray.700')}
+      >
+         <Formik
+            onSubmit={(values) => console.log(values)}
+            validationSchema={validationSchema}
+            initialValues={{
+               name: '',
+               detail: '',
+               start: new Date(),
+               end: new Date(),
+            }}
+         >
+            {({
+               handleChange,
+               handleBlur,
+               handleSubmit,
+               setFieldValue,
+               values,
+               errors,
+               touched,
+            }) => (
+               <>
+                  <FormControl
+                     id='name'
+                     isInvalid={errors.name && touched.name}
+                  >
+                     <FormLabel>Event name</FormLabel>
+                     <Input
+                        id='name'
+                        placeholder='Enter event name'
+                        value={values.name}
+                        onChange={handleChange('name')}
+                     />
+                     <FormErrorMessage>{errors.name}</FormErrorMessage>
+                  </FormControl>
+                  <HStack spacing='6'>
+                     <FormControl
+                        id='start'
+                        isInvalid={errors.start && touched.start}
+                     >
+                        <FormLabel>Start At</FormLabel>
+                        <Flatpickr
+                           style={{ width: '180px' }}
+                           data-enable-time
+                           value={values.start}
+                           onChange={(value) => {
+                              setFieldValue('start', value);
+                              setEndLimit(value);
+                           }}
+                        />
+                        <FormErrorMessage>{errors.start}</FormErrorMessage>
+                     </FormControl>
+                     <FormControl
+                        id='end'
+                        isInvalid={errors.end && touched.end}
+                     >
+                        <FormLabel>Ends At</FormLabel>
+                        <Flatpickr
+                           style={{ width: '180px' }}
+                           data-enable-time
+                           value={values.end}
+                           onChange={handleChange('end')}
+                           onChange={(value) => {
+                              setFieldValue('end', value);
+                           }}
+                        />
+                        <FormErrorMessage>{errors.end}</FormErrorMessage>
+                     </FormControl>
+                  </HStack>
+                  <FormControl
+                     id='detail'
+                     isInvalid={errors.detail && touched.detail}
+                  >
+                     <FormLabel>Detail</FormLabel>
+                     <Textarea
+                        value={values.detail}
+                        onChange={handleChange('detail')}
+                        placeholder='Enter event detail'
+                        size='sm'
+                        maxH='90px'
+                     />
+                     <FormErrorMessage>{errors.detail}</FormErrorMessage>
+                  </FormControl>
+                  <Stack
+                     spacing={4}
+                     direction={['column', 'row']}
+                     w={'full'}
+                     maxW={'md'}
+                     pb='4'
+                  >
+                     <Button
+                        bg={'red.400'}
+                        color={'white'}
+                        w='full'
+                        _hover={{
+                           bg: 'red.500',
+                        }}
+                        onClick={() => {
+                           onClose();
+                        }}
+                     >
+                        Cancel
+                     </Button>
+                     <Button
+                        bg={'blue.400'}
+                        color={'white'}
+                        w='full'
+                        _hover={{
+                           bg: 'blue.500',
+                        }}
+                        onClick={handleSubmit}
+                     >
+                        Add
+                     </Button>
+                  </Stack>
+               </>
+            )}
+         </Formik>
+      </Stack>
+   );
+}
+
+export default AddEvent;
--- client/src/features/Event/pages/ManageEvent/index.jsx
+++ client/src/features/Event/pages/ManageEvent/index.jsx
@@ -1,17 +1,94 @@
-import { Box, SimpleGrid, Stack } from '@chakra-ui/react';
-import React from 'react';
+import { SearchIcon, AddIcon } from '@chakra-ui/icons';
+import {
+   Box,
+   Heading,
+   Stack,
+   Text,
+   HStack,
+   VStack,
+   InputGroup,
+   InputLeftElement,
+   Input,
+   Select,
+   Button,
+   useDisclosure,
+   Image,
+   Menu,
+   MenuButton,
+   MenuItem,
+   MenuList,
+   useToast,
+   SimpleGrid,
+} from '@chakra-ui/react';
+import React, { useState, useEffect } from 'react';
 import EventCard from '../../components/EventCard';
+import AlertDialogBox from '../../../../components/AlertDialogBox';
+import EventModal from '../../components/EventModal';
 
 ManageEvent.propTypes = {};
 
 function ManageEvent(props) {
+   //STATE
+   const [modalData, setModalData] = useState({});
+   //Hook
+   const { isOpen, onOpen, onClose } = useDisclosure();
+   //Toast
+   const toast = useToast();
+
    return (
       <Stack direction={['column', 'row']} spacing='24px' minH='82vh'>
          <Box flex='1' bg='white' boxShadow='xl' rounded='lg' p='5'>
+            <Heading fontSize='25' mb='2'>
+               Manage Event
+            </Heading>
+            <Text mb='6'>Manage all existing events in the hotel</Text>
+            <VStack spacing='5'>
+               <HStack spacing='5' width='full' mb='20px'>
+                  <InputGroup flex='2'>
+                     <InputLeftElement
+                        pointerEvents='none'
+                        children={<SearchIcon color='gray.300' />}
+                     />
+                     <Input
+                        placeholder='Search by event name'
+                        // onChange={(e) =>
+                        //    handleOnSearchChange(e.target.value, 'name')
+                        // }
+                     />
+                  </InputGroup>
+                  <Button
+                     leftIcon={<AddIcon />}
+                     colorScheme='green'
+                     variant='solid'
+                     px='20px'
+                     onClick={() => {
+                        onOpen();
+                        setModalData({
+                           title: 'Add new event',
+                           modalType: 'Add',
+                        });
+                     }}
+                  >
+                     Add
+                  </Button>
+               </HStack>
+            </VStack>
             <SimpleGrid columns={3} spacing={7}>
                <EventCard />
             </SimpleGrid>
          </Box>
+         <EventModal
+            isOpen={isOpen}
+            modalData={modalData}
+            onClose={onClose}
+            // handleAdd={handleAdd}
+            // handleEdit={handleEdit}
+         />
+         <AlertDialogBox
+         // isDeleteAlertOpen={isDeleteAlertOpen}
+         // setIsDeleteAlertOpen={setIsDeleteAlertOpen}
+         // handleOnDelete={handleOnDelete}
+         />
       </Stack>
    );
 }
